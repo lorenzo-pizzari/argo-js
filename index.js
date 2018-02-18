cc851c5cc54c04e8289d2e8dc9bfdcaf217a02e4
Lorenzo Pizzari <lorenzo@pizzari.it>
2018-02-18 18:58:37 +0100

    Mock server + IDE Setup

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: 'localhost',
+  port: 8000
+})
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/healthCheck',
+  handler: function (request, h) {
+    return {status: 'ok'}
+  }
+})
+
+// Start the server
+async function start () {
+  try {
+    // here insert await server.register()
+    await server.start()
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log('Server running at:', server.info.uri)
+}
+
+start()

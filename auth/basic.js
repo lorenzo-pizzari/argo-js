4b82ad746abe31c78c2f0aadc2b0a1c5daf1bf3a
Lorenzo Pizzari <lorenzo@pizzari.it>
2018-02-19 23:12:17 +0100

    #10 Basic Authentication Strategy

---
+++ auth/basic.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs')
+const Boom = require('boom')
+const Basic = require('hapi-auth-basic')
+
+exports.plugin = {
+  name: 'basic-authentication',
+  version: '1.0.0',
+  dependencies: ['hapi-mongodb'],
+  register: basicAuthentication
+}
+
+async function basicAuthentication (server, options) {
+  await server.register(Basic)
+
+  server.auth.strategy('basic', 'basic', {validate})
+  server.auth.default('basic')
+}
+
+async function validate (request, username, password) {
+  const usersColl = request.mongo.db.collection('users')
+  let credentials
+  return usersColl.findOne({email: username})
+    .then(user => {
+      if (!user) throw Boom.unauthorized()
+      credentials = user
+      return bcrypt.compare(password, user.password)
+    })
+    .then(isValid => {
+      return {isValid, credentials: credentials}
+    })
+    .catch(() => {
+      return {isValid: false}
+    })
+}
